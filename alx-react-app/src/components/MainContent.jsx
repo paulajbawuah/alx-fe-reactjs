783a2f8e50069e6d1e751399ba9b630c413957e8
Paula J Bawuah <bawuahpaula9@gmail.com>
2025-09-14 17:00:24 +0100

    Header, MainContent, & Footer added

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,4 +1,4 @@
-function MainContext() {
+function MainContent() {
   return (
     <main>
       <p>I love to visit New York, Paris, and Tokyo.</p>
@@ -6,4 +6,4 @@ function MainContext() {
   );
 }
 
-export default MainContext;
+export default MainContent;
