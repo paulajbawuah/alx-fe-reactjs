610ddff7cad4d18c75e0a4f7f57db4a05edda5be
Paula J Bawuah <bawuahpaula9@gmail.com>
2025-09-14 17:32:16 +0100

    UserProfile added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage.jsx'
 import Header from './components/Header.jsx';
 import MainContent from './components/MainContent.jsx';
 import Footer from './components/Footer.jsx';
+import UserProfile from './components/UserProfile.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,11 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Paula Jacqueline" age="27" bio="Loves music and photography" />
+      <UserProfile name="Bob" age="30" bio="Passionate about coding" />
+      <UserProfile name="Sarah" age="28" bio="Professional chef and food blogger" />
+
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
